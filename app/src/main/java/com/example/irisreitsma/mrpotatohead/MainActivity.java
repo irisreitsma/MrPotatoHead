2f54ecb9fd1b59a915f0245a6bf8c2e0933e0f57
Iris Reitsma <irisreitsma@online.nl>
2018-02-09 23:54:49 +0100

    Initial project

--- app/src/main/java/com/example/irisreitsma/mrpotatohead/MainActivity.java
+++ app/src/main/java/com/example/irisreitsma/mrpotatohead/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.irisreitsma.mrpotatohead;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // check if boxes are clicked
+    public void checkClicked(View v) {
+        Log.d("potato", "checkClicked: ");
+        CheckBox checkbox = (CheckBox) v;
+        boolean checked = checkbox.isChecked();
+        String boxName = checkbox.getText().toString();
+
+        // show the body parts when boxes are clicked
+        if (checked):{
+            ImageView image = findViewById(R.id.checkbox);
+            image.setVisibility(View.VISIBLE);
+        }
+        else {
+            ImageView image = findViewById(R.id.checkbox);
+            image.setVisibility(View.INVISIBLE);
+        }
+    }
+
+
 }
+
