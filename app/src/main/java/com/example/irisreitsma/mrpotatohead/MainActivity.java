f2addde8d367e404ba0697626c39d9bf777c1ffb
Iris Reitsma <irisreitsma@online.nl>
2018-02-15 12:37:00 +0100

    afgetekende eindversie

--- app/src/main/java/com/example/irisreitsma/mrpotatohead/MainActivity.java
+++ app/src/main/java/com/example/irisreitsma/mrpotatohead/MainActivity.java
@@ -9,15 +9,35 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    // lists for comparison
+    int imageID[] = {R.id.eyes, R.id.glasses, R.id.eyebrows, R.id.ears, R.id.arms,
+            R.id.mouth, R.id.moustache, R.id.nose, R.id.hat, R.id.shoes};
+    String checkboxesName[] = {"eyes", "glasses", "eyebrows", "ears", "arms", "mouth",
+            "moustache", "nose", "hat", "shoes"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // reload same state
+        if (savedInstanceState != null) {
+            for (int i = 0; i < imageID.length; i++) {
+                ImageView image = findViewById(imageID[i]);
+                image.setVisibility(savedInstanceState.getInt(checkboxesName[i]));
+            }
+        }
     }
 
-    // lists for comparison
-    int imageID[] = {R.id.eyes, R.id.glasses, R.id.eyebrows, R.id.ears, R.id.arms, R.id.mouth, R.id.moustache, R.id.nose, R.id.hat, R.id.shoes};
-    String checkboxesName[] = {"eyes", "glasses", "eyebrows", "ears", "arms", "mouth", "moustache", "nose", "hat", "shoes"};
+    // preserve state of objects
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        for (int i = 0; i < imageID.length; i++) {
+            ImageView image = findViewById(imageID[i]);
+            outState.putInt(checkboxesName[i], image.getVisibility());
+        }
+    }
 
     // check if boxes are clicked
     public void checkClicked(View v) {
